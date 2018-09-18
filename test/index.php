fc74eafc10bf81671891ce7e8bdb5674fc1067fe
vdevel00 <iamshanuuknow@outlook.com>
2018-09-18 12:56:16 +0530

    Initial commit

---
+++ test/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>google map</title>
+</head>
+<body>
+<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15077.756512185248!2d72.93972414999999!3d19.132246!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1515757670713" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</body>
+</html>
